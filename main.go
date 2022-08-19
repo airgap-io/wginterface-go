cf07e8274dfe177ab818566164dc7d8af340625f
davidmurali <david.m@kriyatec.com>
2022-08-19 20:16:56 +0530

    firewall inbound rule
    set, status, delete for windows

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
+	"airgap.io/wginterface/pkg/firewall"
 	"airgap.io/wginterface/pkg/osquery"
 	"airgap.io/wginterface/pkg/wireguard"
 	_ "airgap.io/wginterface/pkg/wireguard"
@@ -17,24 +20,46 @@ func main() {
 		fmt.Println("less input arguements")
 		return
 	}
-	intfName := strings.Split(filepath.Base(os.Args[1]), ".")
+
 	osName := osquery.DetectOS()
-	if len(intfName[0]) > 0 {
-		var wg wireguard.Wireguard
-		if osName == "windows" {
-			wgWindows := wireguard.WireguardWindows{}
-			wgWindows.Wgname = intfName[0]
-			wgWindows.Wgpath = os.Args[1]
-			wg = wgWindows
-		} else if osName == "darwin" {
-			wgMac := wireguard.WireguardMac{}
-			wgMac.Wgname = intfName[0]
-			wgMac.Wgpath = os.Args[1]
-			wg = wgMac
-		}
-		wg.CreateTunnelInterface()
-		state, _ := wg.GetInterfaceStatus(intfName[0])
-		fmt.Println(state)
-		wg.DeleteTunnelInterface(intfName[0])
+	//firewall
+	if osName == "windows" {
+		fw := firewall.FireWallWindows{}
+		fw.Name = os.Args[1]
+		fw.Action = os.Args[2]
+		fw.Protocol = os.Args[3]
+		fw.Port, _ = strconv.Atoi(os.Args[4])
+		fw.AddNetworkPolicy(fw.Name, fw.Action, fw.Protocol, fw.Port)
+		time.Sleep(10 * time.Second)
+		b, _ := fw.CheckRuleStatus(fw.Name)
+		fmt.Println("status", b)
+		b, _ = fw.DeleteNetworkPolicy(fw.Name)
+		fmt.Println("delete", b)
+
 	}
+	//wireguard
+	{
+		intfName := strings.Split(filepath.Base(os.Args[1]), ".")
+		osName := osquery.DetectOS()
+		if len(intfName[0]) > 0 {
+			var wg wireguard.Wireguard
+			if osName == "windows" {
+				wgWindows := wireguard.WireguardWindows{}
+				wgWindows.Wgname = intfName[0]
+				wgWindows.Wgpath = os.Args[1]
+
+				wg = wgWindows
+			} else if osName == "darwin" {
+				wgMac := wireguard.WireguardMac{}
+				wgMac.Wgname = intfName[0]
+				wgMac.Wgpath = os.Args[1]
+				wg = wgMac
+			}
+			wg.CreateTunnelInterface()
+			state, _ := wg.GetInterfaceStatus(intfName[0])
+			fmt.Println(state)
+			wg.DeleteTunnelInterface(intfName[0])
+		}
+	}
+
 }
---
+++ pkg/firewall/fw_windows.go
@@ -0,0 +1,46 @@
+package firewall
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+)
+
+type FireWallWindows struct {
+	Name string
+	//Group    string
+	Action   string
+	Protocol string
+	Port     int
+}
+
+func (f *FireWallWindows) AddNetworkPolicy(name string, action string, protocol string, port int) error {
+	arguement := "netsh advfirewall firewall add rule name=" + "\"" + f.Name + "\"" + " dir=in action=" + action + " protocol=" + f.Protocol + " localport=" + strconv.Itoa(f.Port)
+	_, err := exec.Command("powershell", arguement).Output()
+	if err != nil {
+		fmt.Println("AddNetworkPolicy Error: ", err)
+	}
+	return err
+}
+
+func (f *FireWallWindows) CheckRuleStatus(name string) (bool, error) {
+	arguement := "netsh advfirewall firewall show rule name=" + "\"" + name + "\""
+	fmt.Println(arguement)
+	_, err := exec.Command("powershell", arguement).Output()
+	if err != nil {
+		//fmt.Println("RuleStatus Error: ", err)
+		return false, err
+	}
+	return true, exec.ErrNotFound
+}
+func (f *FireWallWindows) DeleteNetworkPolicy(name string) (bool, error) {
+	arguement := "netsh advfirewall firewall delete rule name=" + "\"" + name + "\""
+	fmt.Println(arguement)
+	cmd, err := exec.Command("powershell", arguement).Output()
+	fmt.Println(cmd)
+	if err != nil {
+		fmt.Println("DeleteNetworkPolicy Error: ", err)
+		return false, err
+	}
+	return true, exec.ErrNotFound
+}
